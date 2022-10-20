fbbd1976af843fcbc4d7c7b9e9b38df904bffaa1
DienVuong <vuongdien261@gmail.com>
2022-10-20 16:35:47 +0700

    up

--- DSA_STACKANDQUEUE/src/Queue/Node.java
+++ DSA_STACKANDQUEUE/src/Queue/Node.java
@@ -1,2 +1,10 @@
-package Queue;public class Node {
+package Queue;
+
+public class Node {
+    public int key;
+    public Node next;
+    public Node(int key){
+        this.key = key;
+        this.next = null;
+    }
 }
